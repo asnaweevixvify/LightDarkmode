11e2d7d590b543c9a42aeb4c99e90199201d7f42
asnaweevixvify <asnaweeezor@gmail.com>
2025-04-22 19:32:08 +0700

    local

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-function modeChange(){
-    let bodyBack = document.querySelector('.body');
-    let textOn = document.querySelector('.h1-3')
-    let textOn2 = document.querySelector('.h1-4')
-    let nav = document.querySelector('ul')
-    let checkbox = document.querySelector('.check')
-    let lighttext = document.querySelector('.h3')
-    let icons = document.querySelector('#icons')
+let bodyBack = document.querySelector('.body');
+let textOn = document.querySelector('.h1-3')
+let textOn2 = document.querySelector('.h1-4')
+let nav = document.querySelector('ul')
+let checkbox = document.querySelector('.check')
+let lighttext = document.querySelector('.h3')
+let icons = document.querySelector('#icons')
+let check;
 
+window.onload=function(){
+    let modeSave = localStorage.getItem('mode')
+    if(modeSave === 'dark'){
+        checkbox.checked = true
+        modeChange()
+    }
+}
+function modeChange(){
     if(checkbox.checked){
         lighttext.innerText=("โหมดกลางคืน")  
         icons.classList.replace('fa-sun','fa-moon')
+        check = "dark"
     }
     else{
         lighttext.innerText=("โหมดกลางวัน")
         icons.classList.replace('fa-moon','fa-sun')
+        check = "light"
     }
-    
     bodyBack.classList.toggle('darkBody');
     textOn.classList.toggle('darkh1-3')
     textOn2.classList.toggle('darkh1-4')
     nav.classList.toggle('darkul')
-
+    localStorage.setItem('mode',check)
 }
 
 
-//              <i class="fa-solid fa-moon fa-xl"></i>
